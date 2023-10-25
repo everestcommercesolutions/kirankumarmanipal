026fec7353935aa52ba76b1348c05deaad3a63e9
Everest Commerce Solutions <121356683+everestcommercesolutions@users.noreply.github.com>
2023-10-25 09:41:33 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -12,7 +12,7 @@ $(function () {
 
         // if the validator does not prevent form submit
         if (!e.isDefaultPrevented()) {
-            var url = "http://www.everestcommercesolutions.com/astovastu/contact.php";
+            var url = "contact.php";
 
             // POST values in the background the the script URL
             $.ajax({
